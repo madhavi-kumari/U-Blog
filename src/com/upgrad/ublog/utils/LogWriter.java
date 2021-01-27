f9849aaa1c7a47fe6f7ad4674151ce20c6faf287
Madhavi Kumari <roymadhavi333@gmail.com>
2021-01-27 13:13:24 +0530

    Blog Committed

---
+++ src/com/upgrad/ublog/utils/LogWriter.java
@@ -0,0 +1,20 @@
+package com.upgrad.ublog.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * TODO: 5.1. Implement writeLog() method with the following method signature.
+ *  public static void writeLog(String logMessage, String path) throws IOException
+ *  This method should append the log message to the log file that is stored at the
+ *  Input path.
+ */
+
+public class LogWriter {
+    public static void writeLog(String logMessage, String path) throws IOException {
+        FileWriter fr = new FileWriter(path, true);
+        fr.write(logMessage);
+        fr.write('\n');
+        fr.close();
+    }
+}
